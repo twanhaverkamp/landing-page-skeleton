c814f2349a4bef64ceca13b94167b0f3310b5fc2
Twan Haverkamp <twan@mailcampaigns.nl>
2021-02-16 19:35:05 +0100

    Named dataset keys

--- tests/Functional/Controller/RegisterNewCustomerControllerTest.php
+++ tests/Functional/Controller/RegisterNewCustomerControllerTest.php
@@ -75,95 +75,98 @@ class RegisterNewCustomerControllerTest extends WebTestCase
 
     public function getTranslatedFormLabels(): iterable
     {
-        yield [
-            'translatedUrl' => '/en/register-new-customer',
-            'expectedTranslatedLabels' => [
-                'Gender',
-                'Male',
-                'Female',
-                'First name',
-                'Last name',
-                'E-mail address',
-                'Date of birth',
+        return [
+            'An English visitor' => [
+                'translatedUrl' => '/en/register-new-customer',
+                'expectedTranslatedLabels' => [
+                    'Gender',
+                    'Male',
+                    'Female',
+                    'First name',
+                    'Last name',
+                    'E-mail address',
+                    'Date of birth',
+                ],
             ],
-        ];
-
-        yield [
-            'translatedUrl' => '/nl/registreren-nieuwe-klant',
-            'expectedTranslatedLabels' => [
-                'Geslacht',
-                'Man',
-                'Vrouw',
-                'Voornaam',
-                'Achternaam',
-                'E-mailadres',
-                'Geboortedatum',
+            'A Dutch visitor' => [
+                'translatedUrl' => '/nl/registreren-nieuwe-klant',
+                'expectedTranslatedLabels' => [
+                    'Geslacht',
+                    'Man',
+                    'Vrouw',
+                    'Voornaam',
+                    'Achternaam',
+                    'E-mailadres',
+                    'Geboortedatum',
+                ],
             ],
         ];
     }
 
     public function getValidFormData(): iterable
     {
-        yield [
-            'translatedUrl' => '/en/register-new-customer',
-            'translatedRedirectUrl' => '/en/registration-new-customer-confirmed',
-            'translatedButtonValue' => 'Register',
-            'formData' => [
-                'register_new_customer[gender]' => Customer::GENDER_MALE,
-                'register_new_customer[firstName]' => 'John',
-                'register_new_customer[lastName]' => 'Doe',
-                'register_new_customer[emailAddress]' => 'john.doe@example.com',
-                'register_new_customer[dateOfBirth][day]' => 1,
-                'register_new_customer[dateOfBirth][month]' => 1,
-                'register_new_customer[dateOfBirth][year]' => 1970,
+        return [
+            'A valid English registrant' => [
+                'translatedUrl' => '/en/register-new-customer',
+                'translatedRedirectUrl' => '/en/registration-new-customer-confirmed',
+                'translatedButtonValue' => 'Register',
+                'formData' => [
+                    'register_new_customer[gender]' => Customer::GENDER_MALE,
+                    'register_new_customer[firstName]' => 'John',
+                    'register_new_customer[lastName]' => 'Doe',
+                    'register_new_customer[emailAddress]' => 'john.doe@example.com',
+                    'register_new_customer[dateOfBirth][day]' => 1,
+                    'register_new_customer[dateOfBirth][month]' => 1,
+                    'register_new_customer[dateOfBirth][year]' => 1970,
+                ],
             ],
-        ];
-
-        yield [
-            'translatedUrl' => '/nl/registreren-nieuwe-klant',
-            'translatedRedirectUrl' => '/nl/registratie-nieuwe-klant-bevestigd',
-            'translatedButtonValue' => 'Registreren',
-            'formData' => [
-                'register_new_customer[gender]' => Customer::GENDER_FEMALE,
-                'register_new_customer[firstName]' => 'Jane',
-                'register_new_customer[lastName]' => 'Doe',
-                'register_new_customer[emailAddress]' => 'jane.doe@example.com',
-                'register_new_customer[dateOfBirth][day]' => 31,
-                'register_new_customer[dateOfBirth][month]' => 12,
-                'register_new_customer[dateOfBirth][year]' => 1970,
+            'A valid Dutch registrant' => [
+                'translatedUrl' => '/nl/registreren-nieuwe-klant',
+                'translatedRedirectUrl' => '/nl/registratie-nieuwe-klant-bevestigd',
+                'translatedButtonValue' => 'Registreren',
+                'formData' => [
+                    'register_new_customer[gender]' => Customer::GENDER_FEMALE,
+                    'register_new_customer[firstName]' => 'Jane',
+                    'register_new_customer[lastName]' => 'Doe',
+                    'register_new_customer[emailAddress]' => 'jane.doe@example.com',
+                    'register_new_customer[dateOfBirth][day]' => 31,
+                    'register_new_customer[dateOfBirth][month]' => 12,
+                    'register_new_customer[dateOfBirth][year]' => 1970,
+                ],
             ],
         ];
     }
 
     public function getInvalidFormData(): iterable
     {
-        yield [
-            'translatedUrl' => '/en/register-new-customer',
-            'translatedButtonValue' => 'Register',
-            'formData' => [
-                'register_new_customer[firstName]' => 'J0hn',
-                'register_new_customer[lastName]' => 'Do€',
-                'register_new_customer[emailAddress]' => 'malformed-email-address',
+        return [
+            'An invalid English registrant' => [
+                'translatedUrl' => '/en/register-new-customer',
+                'translatedButtonValue' => 'Register',
+                'formData' => [
+                    'register_new_customer[firstName]' => 'J0hn',
+                    'register_new_customer[lastName]' => 'Do€',
+                    'register_new_customer[emailAddress]' => 'malformed-email-address',
+                ],
+                'expectedErrorMessages' => [
+                    'First name contains invalid characters',
+                    'Last name contains invalid characters',
+                    'E-mail address is invalid',
+                ],
             ],
-            'expectedErrorMessages' => [
-                'First name contains invalid characters',
-                'Last name contains invalid characters',
-                'E-mail address is invalid',
-            ],
-        ];
-
-        yield [
-            'translatedUrl' => '/nl/registreren-nieuwe-klant',
-            'translatedButtonValue' => 'Registreren',
-            'formData' => [
-                'register_new_customer[firstName]' => 'J@ne',
-                'register_new_customer[lastName]' => 'Doe_1',
-                'register_new_customer[emailAddress]' => 'malformed-email-address',
-            ],
-            'expectedErrorMessages' => [
-                'Voornaam bevat ongeldige tekens',
-                'Achternaam bevat ongeldige tekens',
-                'E-mailadres is ongeldig',
+            'An invalid Dutch registrant' => [
+                'translatedUrl' => '/nl/registreren-nieuwe-klant',
+                'translatedButtonValue' => 'Registreren',
+                'formData' => [
+                    'register_new_customer[firstName]' => 'J@ne',
+                    'register_new_customer[lastName]' => 'Doe_1',
+                    'register_new_customer[emailAddress]' => 'malformed-email-address',
+                ],
+                'expectedErrorMessages' => [
+                    'Voornaam bevat ongeldige tekens',
+                    'Achternaam bevat ongeldige tekens',
+                    'E-mailadres is ongeldig',
+                ],
             ],
         ];
     }
